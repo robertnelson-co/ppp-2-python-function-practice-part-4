293b863d1220af2a35c73e7157cd0937a7a03e55
robertnelson-co <hello@robertnelson.co>
2023-10-25 21:49:15 -0400

    writing instructions as comments

---
+++ app.py
@@ -0,0 +1,15 @@
+#function called max_num() to find the maximum of three numbers 
+def max_num(arg1, arg2, arg3):
+    biggie = max(arg1, arg2, arg3)
+    print(f"the max number among {arg1}, {arg2}, and {arg3} is {biggie}")
+
+#function called mult_list() to multiply all the numbers in a list
+
+#function called rev_string() to reverse a string
+
+#function called num_within() to check whether a number falls in a given range
+#Accepts number, begining of range, and end of range(inclusive) as arguments
+#Examples: num_within(3,2,4) returns true;  num_within(10,2,5) returns false
+
+#function called pascal() that prints out the first n rows of Pascal's triangle
+#Accepts number n, the number of rows to print
