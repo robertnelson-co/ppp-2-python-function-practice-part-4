e59a97abcd8bbe595641bb720b42eff9ae03da75
robertnelson-co <hello@robertnelson.co>
2023-10-25 22:26:27 -0400

    trying first three functions

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 #function called max_num() to find the maximum of three numbers 
 def max_num(arg1, arg2, arg3):
     biggie = max(arg1, arg2, arg3)
-    print(f"the max number among {arg1}, {arg2}, and {arg3} is {biggie}")
+    print(f"the biggest number among {arg1}, {arg2}, and {arg3} is {biggie}")
 
+max_num(1,1.3,0.9)
 #function called mult_list() to multiply all the numbers in a list
 
+def mult_list(number_list):
+    result = 1
+    for num in number_list:
+        result *= num
+    print(f"After multiplying all the numbers in {number_list}, the result is {result}")
+
+number_list = {1, 2, 3}
+mult_list(number_list)
 #function called rev_string() to reverse a string
+
+def rev_string(input):
+    reversed_string = input [::-1]
+    print(reversed_string)
+
+rev_string("hello")
 
 #function called num_within() to check whether a number falls in a given range
 #Accepts number, begining of range, and end of range(inclusive) as arguments
